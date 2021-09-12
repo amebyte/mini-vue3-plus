1dbd2ac6c6f00a06302669f67cf06940fa796eee
unknown <416212316@qq.com>
2021-09-12 23:09:46 +0800

    fix

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -5,7 +5,7 @@ class ReactiveEffect{
     }
     run() {
         activeEffect = this
-        this._fn()
+        return this._fn()
     }
 }
 const targetMap = new Map()
@@ -33,4 +33,5 @@ let activeEffect
 export function effect(fn) {
     const _effect = new ReactiveEffect(fn)
     _effect.run()
+    return _effect.run.bind(_effect)
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -14,4 +14,16 @@ describe('effect', () => {
         user.age++
         expect(nextAge).toBe(12)
     })
+    it('should return runner when call effect', () => {
+        let foo = 10
+        const runner = effect(() => {
+            foo++
+            return 'foo'
+        })
+        
+        expect(foo).toBe(11)
+        const r = runner()
+        expect(foo).toBe(12)
+        expect(r).toBe('foo')
+    })
 })
