b9963cf4c91e551667d73441dbb0c42a24801e00
unknown <416212316@qq.com>
2021-09-26 20:04:59 +0800

    init dev1

--- src/reactivity/tests/reactive.spec.ts
+++
@@ -1,9 +0,0 @@
-import { reactive } from '../reactive';
-describe('reactive', () => {
-    it('happy path', () => {
-        const original = {foo: 1}
-        const observed = reactive(original)
-        expect(observed).not.toBe(original)
-        expect(observed.foo).toBe(1)
-    })
-})
