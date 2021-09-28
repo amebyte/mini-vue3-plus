2ec31eeedab1ee2b1b1286c299c6e56fa0173bba
coboy <416212316@qq.com>
2021-09-28 14:55:27 +0800

    fix

---
+++ src/reactivity/baseHandler.ts
@@ -0,0 +1,29 @@
+import { track, trigger } from "./effect"
+
+function createGetter(isReadonly = false) {
+    return function get(target, key) {
+        const res = Reflect.get(target, key)
+        if(isReadonly) track(target, key)
+        return res
+    }
+}
+
+function createSetter() {
+    return function set(target, key, val){
+        const res = Reflect.set(target, key, val)
+        trigger(target, key)
+        return res
+    }
+}
+
+export const mutableHanders = {
+    get: createSetter(),
+    set: createSetter()
+}
+
+export const readonlyHanders = {
+    get: createGetter(true),
+    set(target, key, val) {
+        return true
+    }
+}
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,29 +1,9 @@
-import { track, trigger } from "./effect"
-
-function createGetter(isReadonly = false) {
-    return function get(target, key) {
-        const res = Reflect.get(target, key)
-        if(isReadonly) track(target, key)
-        return res
-    }
-}
+import { mutableHanders, readonlyHanders } from "./baseHandler"
 
 export function reactive(raw) {
-    return new Proxy(raw, {
-        get: createGetter(),
-        set(target, key, val){
-            const res = Reflect.set(target, key, val)
-            trigger(target, key)
-            return res
-        }
-    })
+    return new Proxy(raw, mutableHanders)
 }
 
 export function readonly(raw) {
-    return new Proxy(raw, {
-        get: createGetter(true),
-        set(target, key, val) {
-            return true
-        }
-    })
+    return new Proxy(raw, readonlyHanders)
 }
