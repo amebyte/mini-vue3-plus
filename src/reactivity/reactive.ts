97a1137ef70db4ba66cbffd2d66ed8f5f0e29a2a
coboy <416212316@qq.com>
2021-09-28 14:33:14 +0800

    readonly init

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -14,3 +14,15 @@ export function reactive(raw) {
         }
     })
 }
+
+export function readonly(raw) {
+    return new Proxy(raw, {
+        get(target, key) {
+            const res = Reflect.get(target, key)
+            return res
+        },
+        set(target, key, val) {
+            return true
+        }
+    })
+}
