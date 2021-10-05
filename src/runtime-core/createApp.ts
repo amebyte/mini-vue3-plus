906ebd7da22a3b773f683a789540eb7cbe4156e6
unknown <416212316@qq.com>
2021-10-05 22:26:24 +0800

    fix

--- src/runtime-core/createApp.ts
+++ src/runtime-core/createApp.ts
@@ -1,10 +1,11 @@
+import { render } from "./renderer"
 import { createVNode } from "./vnode"
 
 export function createApp(rootComponent) {
     return {
         mount(rootComponent) {
             const vnode = createVNode(rootComponent)
-            render(vnode, rootContainer)
+            render(vnode, rootComponent)
         }
     }
 }
