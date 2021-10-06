540036910c938307c916f15c9b0c6eda09737f12
unknown <416212316@qq.com>
2021-10-06 16:12:05 +0800

    fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,15 @@
+import pkg from './package.json'
 import typescript from "@rollup/plugin-typescript"
 export default {
     input: "./src/index.ts",
     output: [
         {
             format: "cjs",
-            file: "lib/mini-vue.cjs.js"
+            file: pkg.main
         },
         {
             format: "es",
-            file: "lib/mini-vue.esm.js"
+            file: pkg.module
         }
     ],
     plugins:[typescript()]
