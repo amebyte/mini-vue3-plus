fbdea79ae0c77d08411760aa98b95f5e2326e983
unknown <416212316@qq.com>
2021-10-10 17:05:30 +0800

    fix

--- src/runtime-core/componentPublicInstance.ts
+++ src/runtime-core/componentPublicInstance.ts
@@ -1,12 +1,20 @@
 
+const publicPropertiesMap = {
+    $el: i => i.vnode.el
+}
+
 export const publicInstanceProxyHandlers = {
     get({ _: instance}, key) {
         const { setupState } = instance
         if(key in setupState) {
             return setupState[key]
         }
-        if(key === '$el') {
-            return instance.vnode.el
+        // if(key === '$el') {
+        //     return instance.vnode.el
+        // }
+        const publicGetter = publicPropertiesMap[key]
+        if(publicGetter) {
+            return publicGetter(instance)
         }
     }
 }
