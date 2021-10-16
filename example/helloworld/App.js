fb025788c56b8c9dcabcab6ac1eed8b43a9b271d
unknown <416212316@qq.com>
2021-10-16 23:06:27 +0800

    update 实现注册事件功能

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -1,14 +1,30 @@
-import { h } from "../../lib/mini-vue.esm.js"
+import { h } from '../../lib/mini-vue.esm.js'
 window.self = null
 export const App = {
-    render() {
-        window.self = this
-        // return h("div",{ id: 'root', class: ['red', 'green']}, "hi," + this.msg)
-        return h("div",{ id: 'root', class: ['red', 'green'], onClick(){ console.log("click") }}, [h("p", {class: "red"}, "hi"), h("span",{class:"green"}, "this is a " + this.msg)])
-    },
-    setup() {
-        return {
-            msg: 'mini-vue'
+  render() {
+    window.self = this
+    // return h("div",{ id: 'root', class: ['red', 'green']}, "hi," + this.msg)
+    return h(
+      'div',
+      {
+        id: 'root',
+        class: ['red', 'green'],
+        onClick() {
+          console.log('click')
+        },
+        onMousedown() {
+            console.log("mousedown")
         }
+      },
+      [
+        h('p', { class: 'red' }, 'hi'),
+        h('span', { class: 'green' }, 'this is a ' + this.msg),
+      ]
+    )
+  },
+  setup() {
+    return {
+      msg: 'mini-vue',
     }
+  },
 }
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -48,8 +48,10 @@ function mountElement(vnode: any, container: any) {
     const { props } = vnode
     for(const key in props) {
         const val = props[key]
-        if(key === 'onClick') {
-            el.addEventListener('click', val)
+        const isOn = (key: string) => /^on[A-Z]/.test(key) 
+        if(isOn(key)) {
+            const event = key.slice(2).toLowerCase()
+            el.addEventListener(event, val)
         } else {
             el.setAttribute(key, val)
         }
