a64e6e0c08242163c0c429a9588ecd79c174d405
unknown <416212316@qq.com>
2021-10-16 22:20:18 +0800

    实现 shapeFlags

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,5 +1,6 @@
 import { visitNode } from "typescript"
 import { isObject } from "../shared"
+import { ShapeFlags } from "../shared/ShapeFlags"
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode: any, container: any) {
@@ -8,9 +9,10 @@ export function render(vnode: any, container: any) {
 
 function patch(vnode: any, container: any) {
     console.log('vnode', vnode.type)
-    if(typeof vnode.type === 'string') {
+    const { shapeFlag } = vnode
+    if(shapeFlag & ShapeFlags.ELEMENT) {
         processElement(vnode, container)
-    } else if(isObject(vnode.type)) {
+    } else if(shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
         processComponent(vnode, container)
     }
 }
@@ -36,10 +38,10 @@ function processElement(vnode: any, container: any) {
 
 function mountElement(vnode: any, container: any) {
     const el = (vnode.el = document.createElement(vnode.type))
-    const {children} = vnode
-    if(typeof children === 'string') {
+    const {children, shapeFlag} = vnode
+    if(shapeFlag & ShapeFlags.TEXT_CHILDREN) {
         el.textContent = children
-    } else if(Array.isArray(children)) {
+    } else if(shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
         mountChildren(vnode, el)
     }
     const { props } = vnode
