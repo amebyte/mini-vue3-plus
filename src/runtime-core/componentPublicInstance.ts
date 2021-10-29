224913f84410e315d7a2e9b25e375d2e8b51087f
unknown <416212316@qq.com>
2021-10-29 20:50:01 +0800

    实现组件 props 逻辑

--- src/runtime-core/componentPublicInstance.ts
+++ src/runtime-core/componentPublicInstance.ts
@@ -7,9 +7,6 @@ const publicPropertiesMap = {
 export const publicInstanceProxyHandlers = {
     get({ _: instance}, key) {
         const { setupState, props } = instance
-        // if(key in setupState) {
-        //     return setupState[key]
-        // }
 
         if(hasOwn(setupState, key)) {
             return setupState[key]
@@ -17,9 +14,6 @@ export const publicInstanceProxyHandlers = {
             return props[key]
         }
 
-        // if(key === '$el') {
-        //     return instance.vnode.el
-        // }
         const publicGetter = publicPropertiesMap[key]
         if(publicGetter) {
             return publicGetter(instance)
