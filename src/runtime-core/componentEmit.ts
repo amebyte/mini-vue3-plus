5841e894c237ccbed74f8f4e44dacb0443bf2727
unknown <416212316@qq.com>
2021-10-29 22:12:05 +0800

    TPP next

--- src/runtime-core/componentEmit.ts
+++ src/runtime-core/componentEmit.ts
@@ -3,6 +3,17 @@ export function emit(instance, event) {
     const { props } = instance
     // TPP
     // 先去写一个特定的行为 =》 重构成通用的行为
-    const handler = props['onAdd']
+
+    const capitalize = (str: string) => {
+        return str.charAt(0).toUpperCase() + str.slice(1)
+    }
+
+    const toHandlerKey = (str: string) => {
+        return str ? 'on' + capitalize(event) : ''
+    }
+
+    const handlerName = toHandlerKey(event)
+
+    const handler = props[handlerName]
     handler && handler()
 }
