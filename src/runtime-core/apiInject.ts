6f30c3c4b64f1bb9d9f8f67edaf2683a4c0a7e3e
unknown <416212316@qq.com>
2021-12-11 22:59:01 +0800

    注意： 按引用传递是传递对象的引用，而按共享传递是传递对象的引用的副本

--- src/runtime-core/apiInject.ts
+++ src/runtime-core/apiInject.ts
@@ -1,12 +1,12 @@
 import { getCurrentInstance } from "./component"
 
-
 export function provide(key, value) {
     const currentInstance: any = getCurrentInstance()
     if(currentInstance) {
-        let { provides } = currentInstance
+        let provides = currentInstance.provides
         const parentProvides = currentInstance.parent.provides
         if(provides === parentProvides) {
+            // Object.create() es6创建对象的另一种方式，可以理解为继承一个对象, 添加的属性是在原型下。
             provides = currentInstance.provides = Object.create(parentProvides)
         }
         provides[key] = value
