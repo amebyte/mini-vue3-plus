02407439bdf73678b03c1b4e4d1798712c3e8069
unknown <416212316@qq.com>
2021-12-21 21:48:38 +0800

    fix

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -164,24 +164,50 @@ import { ref, h } from "../../lib/mini-vue.esm.js";
 // a,b,(c,e),f,g
 // a,b,(e,c,d),f,g
 // d 节点在老的节点中不存在，新的里面存在，所以需要创建
-const prevChildren = [
-  h("p", { key: "A" }, "A"),
-  h("p", { key: "B" }, "B"),
-  h("p", { key: "C" }, "C"),
-  h("p", { key: "E" }, "E"),
-  h("p", { key: "F" }, "F"),
-  h("p", { key: "G" }, "G"),
-];
+// const prevChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C" }, "C"),
+//   h("p", { key: "E" }, "E"),
+//   h("p", { key: "F" }, "F"),
+//   h("p", { key: "G" }, "G"),
+// ];
 
-const nextChildren = [
-  h("p", { key: "A" }, "A"),
-  h("p", { key: "B" }, "B"),
-  h("p", { key: "E" }, "E"),
-  h("p", { key: "C" }, "C"),
-  h("p", { key: "D" }, "D"),
-  h("p", { key: "F" }, "F"),
-  h("p", { key: "G" }, "G"),
-];
+// const nextChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "E" }, "E"),
+//   h("p", { key: "C" }, "C"),
+//   h("p", { key: "D" }, "D"),
+//   h("p", { key: "F" }, "F"),
+//   h("p", { key: "G" }, "G"),
+// ];
+
+// 综合例子
+// a,b,(c,d,e,z),f,g
+// a,b,(d,c,y,e),f,g
+
+const prevChildren = [
+    h("p", { key: "A" }, "A"),
+    h("p", { key: "B" }, "B"),
+    h("p", { key: "C" }, "C"),
+    h("p", { key: "D" }, "D"),
+    h("p", { key: "E" }, "E"),
+    h("p", { key: "Z" }, "Z"),
+    h("p", { key: "F" }, "F"),
+    h("p", { key: "G" }, "G"),
+  ];
+  
+  const nextChildren = [
+    h("p", { key: "A" }, "A"),
+    h("p", { key: "B" }, "B"),
+    h("p", { key: "D" }, "D"),
+    h("p", { key: "C" }, "C"),
+    h("p", { key: "Y" }, "Y"),
+    h("p", { key: "E" }, "E"),
+    h("p", { key: "F" }, "F"),
+    h("p", { key: "G" }, "G"),
+  ];
   
   export default {
     name: "ArrayToArray",
