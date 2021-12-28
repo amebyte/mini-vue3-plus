1979a7241b10bde1d22acb5fad12a95a55747cbe
unknown <416212316@qq.com>
2021-12-28 22:24:11 +0800

    componentUpdate

---
+++ example/componentUpdate/Child.js
@@ -0,0 +1,9 @@
+   
+import { h } from "../../lib/mini-vue.esm.js";
+export default {
+  name: "Child",
+  setup(props, { emit }) {},
+  render(proxy) {
+    return h("div", {}, [h("div", {}, "child - props - msg: " + this.$props.msg)]);
+  },
+};
