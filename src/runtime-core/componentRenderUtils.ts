2c22ef7df4ff15e50f7edaff62c309908d61ea6f
coboy <416212316@qq.com>
2022-04-15 11:50:01 +0800

    notes

--- src/runtime-core/componentRenderUtils.ts
+++ src/runtime-core/componentRenderUtils.ts
@@ -5,8 +5,10 @@ export function renderComponentRoot(
   ) {
     const { proxy, render } = instance
     let result
+    // 返回上一个实例对象
     const prev = setCurrentRenderingInstance(instance)
     result = render.call(proxy)
+    // 再设置当前的渲染对象上一个，具体场景是嵌套循环渲染的时候，渲染完子组件，再去渲染父组件
     setCurrentRenderingInstance(prev)
     return result
 }
