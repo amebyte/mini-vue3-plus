65d93ab198690a526c4ed4b0cb9858895857e381
coboy <416212316@qq.com>
2022-04-19 15:45:38 +0800

    template ref

--- example/TemplateRefs/App.js
+++ example/TemplateRefs/App.js
@@ -1,38 +1,71 @@
 import { h, ref, nextTick } from '../../lib/mini-vue.esm.js'
 import { Foo } from './Foo.js'
-window.self = null
-export const App = {
-  render() {
-    window.self = this
+
+// string
+// export const App = {
+//   render() {
     
-    return h(
-      'div',
-      {
-        onClick: this.refHandler,
-        ref:'elmentRef'
-      },
-      [
-        h(Foo, { ref: 'fooRef'})
-      ]
-    )
-  },
-  setup() {
-    const fooRef = ref(null)
-    const elmentRef = ref(null)
-    const refHandler = () => {
-        console.log('fooRef', fooRef)
-        fooRef.value.emitAdd()
-        console.log('elmentRef', elmentRef.value)
-    }
-    nextTick(() => {
-        console.log('nextTickfooRef', fooRef)
-        fooRef.value.emitAdd()
-        console.log('nextTickelmentRef', elmentRef.value)
-    })
-    return {
-      fooRef,
-      refHandler,
-      elmentRef
-    }
-  },
-}
+//     return h(
+//       'div',
+//       {
+//         onClick: this.refHandler,
+//         ref:'elmentRef'
+//       },
+//       [
+//         h(Foo, { ref: 'fooRef'})
+//       ]
+//     )
+//   },
+//   setup() {
+//     const fooRef = ref(null)
+//     const elmentRef = ref(null)
+//     const refHandler = () => {
+//         console.log('fooRef', fooRef)
+//         fooRef.value.emitAdd()
+//         console.log('elmentRef', elmentRef.value)
+//     }
+//     nextTick(() => {
+//         console.log('nextTickfooRef', fooRef)
+//         fooRef.value.emitAdd()
+//         console.log('nextTickelmentRef', elmentRef.value)
+//     })
+//     return {
+//       fooRef,
+//       refHandler,
+//       elmentRef
+//     }
+//   },
+// }
+
+// ref
+export const App = {
+    render() {
+      
+      return h(
+        'div',
+        {
+          onClick: this.refHandler,
+          ref: this.refKey
+        },
+        [
+          h(Foo, { })
+        ]
+      )
+    },
+    setup() {
+      const fooEl = ref(null)
+      const barEl = ref(null)
+      const refKey = ref('foo')
+      const refHandler = () => {
+      }
+      nextTick(() => {
+          console.log('nextTickfooEl', fooEl)
+      })
+      return {
+        foo:fooEl,
+        bar: barEl,
+        refHandler,
+        refKey
+      }
+    },
+  }
