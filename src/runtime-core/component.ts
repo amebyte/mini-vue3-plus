14d36aba6812f68265ccbb2cf921895637278951
coboy <416212316@qq.com>
2022-04-24 14:30:36 +0800

    LifecycleHooks

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -8,6 +8,23 @@ import { initSlots } from "./componentSlots"
 
 let currentInstance = null
 
+export const enum LifecycleHooks {
+    BEFORE_CREATE = 'bc',
+    CREATED = 'c',
+    BEFORE_MOUNT = 'bm',
+    MOUNTED = 'm',
+    BEFORE_UPDATE = 'bu',
+    UPDATED = 'u',
+    BEFORE_UNMOUNT = 'bum',
+    UNMOUNTED = 'um',
+    DEACTIVATED = 'da',
+    ACTIVATED = 'a',
+    RENDER_TRIGGERED = 'rtg',
+    RENDER_TRACKED = 'rtc',
+    ERROR_CAPTURED = 'ec',
+    SERVER_PREFETCH = 'sp'
+}
+
 export function createComponentInstance(vnode: any, parent) {
     let component = {
         vnode,
