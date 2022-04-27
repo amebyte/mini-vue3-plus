ec0969565ec65373204d20108db57929fd6e4e81
coboy <416212316@qq.com>
2022-04-27 17:30:09 +0800

    notes

--- src/runtime-core/apiLifecycle.ts
+++ src/runtime-core/apiLifecycle.ts
@@ -1,15 +1,18 @@
 import { currentInstance, LifecycleHooks } from "./component"
 
+// injectHook是一个闭包函数，通过闭包保存当前生命周期Hooks在哪个组件实例上
 export function injectHook(type, hook, target) {
     if(target) {
+        // 把各个生命周期的Hooks函数挂载到组件实例上，并且是一个数组，因为可能你会多次调用同一个生命周期函数
         const hooks = target[type] || (target[type] = [])
+        // 把生命周期函数进行包装并且把包装函数缓存在__weh上
         const wrappedHook =
         hook.__weh ||
         (hook.__weh = (...args: unknown[]) => {
           if (target.isUnmounted) {
             return
           }
-
+            // 执行生命周期Hooks函数
             const  res = args ? hook(...args) : hook()
 
           return res
@@ -21,6 +24,8 @@ export function injectHook(type, hook, target) {
     }
 }
 
+// 创建生命周期函数，target,表示该生命周期Hooks函数被绑定到哪个组件实例上，默认是当前工作的组件实例。
+// createHook是一个闭包函数，通过闭包缓存当前是属于哪个生命周期的Hooks
 export const createHook = (lifecycle) => (hook, target = currentInstance) => injectHook(lifecycle, hook, target)
 
 export const onBeforeMount = createHook(LifecycleHooks.BEFORE_MOUNT)
