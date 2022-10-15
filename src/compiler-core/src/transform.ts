b3201767b8ae8f21e29ac42a59aa4ec12f6d92dc
unknown <416212316@qq.com>
2022-10-16 01:19:23 +0800

    fix

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,18 +1,30 @@
 import { NodeTypes } from "./ast"
 
-export function transform(root) {
-    traverseNode(root)
+export function transform(root, options) {
+    const context = createTransformContext(root, options)
+    traverseNode(root, context)
 }
 
-function traverseNode(node: any) {
-    const children = node.children
-    if(node.type ===  NodeTypes.TEXT) {
-        node.content = node.content + " mini-vue"
+function traverseNode(node: any, context) {
+    const nodeTransforms = context.nodeTransforms
+    for(let i = 0; i < nodeTransforms.length; i++) {
+        const transform = nodeTransforms[i]
+        transform(node)
     }
+    const children = node.children
+    
     if(children) {
         for(let i = 0; i < children.length; i++){
             const node = children[i]
-            traverseNode(node)
+            traverseNode(node, context)
         }
     }
+}
+
+function createTransformContext(root: any, options: any) {
+    const context = {
+        root,
+        nodeTransforms: options.nodeTransforms || []
+    }
+    return context
 }
--- src/compiler-core/tests/transform.spec.ts
+++ src/compiler-core/tests/transform.spec.ts
@@ -1,10 +1,18 @@
 import { transform } from "../src/transform"
 import { baseParse } from "../src/parse"
+import { NodeTypes } from "../src/ast"
 
 describe('transform', () => {
     it("happy path", () => {
         const ast = baseParse('<div>hi,{{message}}</div>')
-        transform(ast)
+        const plugin = (node) => {
+            if(node.type === NodeTypes.TEXT) {
+                node.content = node.content + " mini-vue"
+            }
+        }
+        transform(ast, {
+            nodeTransforms: [plugin]
+        })
         const nodeText = ast.children[0].children[0]
         expect(nodeText.content).toBe("hi, mini-vue")
     })
