1b59c7716e6487b99207e03a1e99e1a654f85a9e
unknown <416212316@qq.com>
2022-10-15 21:51:20 +0800

    simple interpolation

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,3 +1,5 @@
+import { NodeTypes } from "./ast"
+
 export function baseParse (content: string) {
     const context = createParserContext(content)
     return createRoot(parseChildren(context)) 
@@ -5,19 +7,35 @@ export function baseParse (content: string) {
 
 function parseChildren(context) {
     const nodes: any = []
-    const node = parseInterpolation()
+    let node
+    if(context.source.startsWith("{{")) {
+        node = parseInterpolation(context)
+    }
     nodes.push(node)
     return nodes
 }
 
-function parseInterpolation() {
+function parseInterpolation(context) {
+    const openDelimiter = '{{'
+    const closeDelimiter = '}}'
+    const closeIndex = context.source.indexOf(closeDelimiter, openDelimiter.length)
+    advanceBy(context, openDelimiter.length)
+    const rawContentLength = closeIndex - openDelimiter.length
+    const rawContent = context.source.slice(0, rawContentLength)
+    const content = rawContent.trim() 
+    advanceBy(context, rawContentLength + closeDelimiter.length)
+
     return {
-        type: "interpolation",
+        type: NodeTypes.INTERPOLATION,
         content: {
-            type: "simple_expression",
-            content: "message"
+            type: NodeTypes.SIMPLE_EXPRESSION,
+            content: content
         }
     }
+}
+
+function advanceBy(context: any, length: number) {
+    context.source = context.source.slice(length)
 }
 
 function createRoot(children) {
--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -1,13 +1,14 @@
+import { NodeTypes } from "../src/ast"
 import { baseParse } from "../src/parse"
 describe('Parse', () => {
     describe('interpolation', () => {
         test('simple interpolation', () => {
-            const ast = baseParse("{{message}}")
+            const ast = baseParse("{{  message    }}")
             // root
             expect(ast.children[0]).toStrictEqual({
-                type: "interpolation",
+                type: NodeTypes.INTERPOLATION,
                 content: {
-                    type: "simple_expression",
+                    type: NodeTypes.SIMPLE_EXPRESSION,
                     content: "message"
                 }
             })
