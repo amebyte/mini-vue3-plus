ed982ff5bb7350b572180b2d1fe28d85966e3050
unknown <416212316@qq.com>
2022-10-23 09:02:17 +0800

    fix

--- src/compiler-core/src/ast.ts
+++ src/compiler-core/src/ast.ts
@@ -1,12 +1,12 @@
 import { CREATE_ELEMENT_VNODE } from "./runtimeHelpers"
 
 export const enum NodeTypes {
-    INTERPOLATION,
-    SIMPLE_EXPRESSION,
-    ELEMENT,
-    TEXT,
-    ROOT,
-    COMPOUND_EXPRESSION
+    INTERPOLATION, // 插值
+    SIMPLE_EXPRESSION, // 简单表达式
+    ELEMENT, // 元素
+    TEXT, // 文本
+    ROOT, // 根节点
+    COMPOUND_EXPRESSION // 复合
 }
 
 export function createVNodeCall(context, tag, props, children) {
